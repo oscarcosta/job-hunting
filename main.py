360020b470c77874bbb5c88b508c82e023101748
Oscar Costa <oscarcosta@gmail.com>
2022-08-01 09:44:23 -0300

    Adjusting status and dates columns

--- main.py
+++ main.py
@@ -1,43 +1,7 @@
 import pandas as pd
 import numpy as np
 import plotly
-from datetime import date
-
-
-# https://medium.com/kenlok/how-to-create-sankey-diagrams-from-dataframes-in-python-e221c1b4d6b0
-def gen_sankey(df, cat_cols=[], value_cols='', title='Sankey Diagram'):
-    # maximum of 6 value cols -> 6 colors
-    color_palette = ['#4B8BBE', '#306998', '#FFE873', '#FFD43B', '#646464']
-    label_list = []
-    color_num_list = []
-    for cat_col in cat_cols:
-        label_list_temp = df[cat_col].unique().tolist()
-        color_num_list.append(len(label_list_temp))
-        label_list = label_list + label_list_temp
-
-    # remove duplicates from label_list
-    label_list = list(dict.fromkeys(label_list))
-
-    # define colors based on number of levels
-    color_list = []
-    for idx, color_num in enumerate(color_num_list):
-        color_list = color_list + [color_palette[idx]] * color_num
-
-    # add default value col if not defined
-    if value_cols == '':
-        df['value_col'] = 1
-        value_cols = 'value_col'
-
-    # transform df into a source-target pair
-    for i in range(len(cat_cols) - 1):
-        if i == 0:
-            source_target_df = df[[cat_cols[i], cat_cols[i + 1], value_cols]]
-            source_target_df.columns = ['source', 'target', 'count']
-        else:
-            temp_df = df[[cat_cols[i], cat_cols[i + 1], value_cols]]
-            temp_df.columns = ['source', 'target', 'count']
-            source_target_df = pd.concat([source_target_df, temp_df])
-        source_target_df = source_target_df.groupby(['source', 'target']).agg({'count': 'sum'}).reset_index()
+from datetime import datetime, timedelta
 
 from sankey import gen_sankey
 
@@ -56,7 +20,7 @@ def plot_by_status(df):
     # print(df.to_string())
 
     fig = gen_sankey(df, cat_cols=['Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5'],
-                     title='Job Hunting Update - ' + date.today().strftime("%d/%m/%Y"))
+                     title='Job Hunting Update - ' + datetime.today().strftime("%d/%m/%Y"))
     plotly.offline.plot(fig, validate=False)
 
 
@@ -66,18 +30,24 @@ def plot_status_by_items(df, items, remove_recruiters=True):
         df.query("`Status` != 'Recruiter'", inplace=True)
 
     fig = gen_sankey(df, cat_cols=['Step 1'] + items + ['Status'],
-                     title='Job Hunting Status by ' + str(items) + ' - ' + date.today().strftime("%d/%m/%Y"))
+                     title='Job Hunting Status by ' + str(items) + ' - ' + datetime.today().strftime("%d/%m/%Y"))
     plotly.offline.plot(fig, validate=False)
 
 
 if __name__ == '__main__':
     # read the data file
     df = pd.read_csv('data/Job Hunting - Sheet1.csv', sep=',', header=0,
-                     usecols=['Date', 'Position', 'Technology', 'Ref/Site',
+                     usecols=['Date', 'Position', 'Technology', 'Ref/Site', 'Last Interation',
                               'Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5', 'Status'])
 
-    # adjust status column
-    df.loc[df["Status"] == "?", "Status"] = 'No Response'
+    # adjust status and dates columns
+    two_weeks = datetime.today() - timedelta(weeks=2)
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+    df['Last Interation'] = pd.to_datetime(df['Last Interation'], dayfirst=True)
+    df.loc[(df['Status'] == "?") & (df['Last Interation'] <= two_weeks), 'Status'] = 'No Response'
+    df.loc[(df['Status'] == "?") & (df['Last Interation'] > two_weeks), 'Status'] = 'Waiting'
+
+    # print(df.to_string())
 
     plot_by_status(df)
     # plot_status_by_items(df, ['Ref/Site'], remove_recruiters=False)
